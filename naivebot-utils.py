5c7f79f5de6fd4fef321fcb2ef84b2292a44b3de
Sunbread <xh3061895@gmail.com>
2018-07-03 21:29:26 +0800

    Add utils of NaiveBot

---
+++ naivebot-utils.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+'The utilities of NaiveBot'
+
+__author__ = 'Sunbread'
+
+def has_command(text, command):
+    _text = text.lower()
+    _command = command.lower()
+    if len(_text) < len(_command) + 1:
+        return False
+    if _text[0] != '/':
+        return False
+    if _text[1:len(_command)+1] != _command:
+        return False
+    if len(_text) > len(_command) + 1:
+        if _text[len(_command)+1] != ' ':
+            return False
+    return True
